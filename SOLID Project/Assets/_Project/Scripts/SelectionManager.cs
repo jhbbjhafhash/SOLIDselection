ff1f6e7c0594f01a99bb1af3c0b8cb7f62c719f3
Nathaniel <156957820+jhbbjhafhash@users.noreply.github.com>
2025-11-07 11:50:10 -0700

    added project file

---
+++ SOLID Project/Assets/_Project/Scripts/SelectionManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+
+    private Transform _selection;
+
+    private HighlightSelectionResponse selectionResponse;
+
+    private void Awake()
+    {
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+    }
+
+    private void Update()
+    {
+        if (_selection != null)
+        {
+            _selectionResponse.OnSelect(_selection);
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        _selection = null;
+        if (Physics.Raycast(ray, out var hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                _selection = selection;
+            }
+        }
+
+        if (_selection != null)
+        {
+            _selectionResponse.OnDeselect(_selection);
+        }
+    }
+}
+
+internal class HighlightSelectionResponse : Monobehavior
+{
+    [SerializeField] public Material highlightMaterial;
+    [SerializeField] public Material defaultMaterial;
+
+    private void OnSelect(Transform selection)
+    {
+        var selectionRenderer = _selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.material = _selectionResponse.defaultMaterial;
+        }
+    }
+
+    private void OnDeselect(Transfrom selection)
+    {
+        var selectionRenderer = _selection.GetComponent<Renderer>();
+        if (selectionRenderer != null)
+        {
+            selectionRenderer.material = _selectionResponse.highlightMaterial;
+        }
+    }
+}
